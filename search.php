889301c3859286cd4b5259a023a03423cac73f24
balasss <47716534+balasss@users.noreply.github.com>
2019-03-12 16:41:15 +0530

    Update search.php

--- search.php
+++ search.php
@@ -5,14 +5,19 @@ Date	   :
 File Name  :search.php 
 Desc       : 
 ****************************/
-
+error_reporting(E_ALL);
+ini_set("error_reporting", E_ALL);
 require_once('MysqliDb.php');
 include 'db.php';
 	$action=$_REQUEST['action'];
 	$id=$_REQUEST['id'];
 		$city = $db->where ("Name",$id);
-		$city = $db->get ("city", null);
-		$result=print_r ($city);
-//echo implode(",",$result);
- 
-?>
+		$result = $db->getOne ("city");
+
+		//implode("<br/> ",$result);
+		echo '<br/> ID: '.$result['ID'];
+	        echo '<br/> Name: '.$result['Name'];
+		echo '<br/> CountryCode: '.$result['CountryCode'];
+	        echo '<br/> District: '.$result['District'];
+		echo '<br/> Population: '.$result['Population'];
+ ?>
